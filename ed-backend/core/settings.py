2b2887219772a452d116f362761cf2e2f1eae7c0
Valeria Avila <130493581+boulangerie0v0@users.noreply.github.com>
2026-08-18 11:51:41 -0700

    cors setup again

--- ed-backend/core/settings.py
+++ ed-backend/core/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import corsheaders
 
 import dj_database_url
 from django.core.exceptions import ImproperlyConfigured
