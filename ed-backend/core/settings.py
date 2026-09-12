2953ae0db643c770df6ca2bdb91d7faed0e032b2
Valeria Avila <130493581+boulangerie0v0@users.noreply.github.com>
2026-09-12 10:12:31 -0700

    Revert "login/signup validation"
    This reverts commit 9f0a4b1eff1cd3ed9886589ee0d1ea7a2b74c3b3.
    
    rollback fro email verification, broke code

--- ed-backend/core/settings.py
+++ ed-backend/core/settings.py
@@ -213,17 +213,3 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
-
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-ALLOWED_EMAIL_DOMAINS = [
-    "gmail.com",
-    "yahoo.com",
-    "outlook.com",
-    "hotmail.com",
-    "icloud.com",
-    "aol.com",
-    "protonmail.com",
-]
-
-VERIFICATION_CODE_TTL_MINUTES = 10
--- ed-backend/users/serializers.py
+++ ed-backend/users/serializers.py
@@ -1,43 +1,21 @@
-from django.conf import settings
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
-
-def validate_email_domain(value):
-    domain = value.split("@")[-1].lower()
-    if domain not in [d.lower() for d in settings.ALLOWED_EMAIL_DOMAINS]:
-        raise serializers.ValidationError(
-            f"Please use an email from one of: {', '.join(settings.ALLOWED_EMAIL_DOMAINS)}"
-        )
-
-
 class SignUpSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(validators=[validate_email_domain])
-
     class Meta:
         model = User
         fields = ["email", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
-
-    def validate_email(self, value):
-        if User.objects.filter(email__iexact=value).exists():
-            raise serializers.ValidationError(
-                "An account with this email already exists."
-            )
-        return value
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
             email=validated_data["email"],
             username=validated_data["username"],
-            password=validated_data["password"],
+            password=validated_data["password"]
         )
-        user.is_active = False  # locked until they verify their email code
-        user.save()
         return user
-
-
+    
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     password = serializers.CharField(required=True, write_only=True)
@@ -52,6 +30,6 @@ class LoginSerializer(serializers.Serializer):
                 raise serializers.ValidationError("Incorrect username or password")
         else:
             raise serializers.ValidationError("Username and password required")
-
+        
         data["user"] = user
         return data
